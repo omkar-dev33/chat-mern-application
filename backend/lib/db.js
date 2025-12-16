e616de96a7c6ef726e0c1ae61bfb93920fedba80
Omkar Sathe <satheomkar71@gmail.com>
2025-12-16 17:32:01 +0530

    error handle

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,4 +1,4 @@
-import  mongoose  from "mongoose"
+import mongoose from "mongoose"
 // const URL = process.env.MONGODB_URL;
 
 export const connectDB = async () => {
@@ -7,7 +7,7 @@ export const connectDB = async () => {
         console.log(`Mongodb connected : ${conn.connection.host}`);
 
     } catch (err) {
-        console.log(`Mongodb not connected : err` , err);
+        console.log(`Mongodb not connected : err` + err);
     }
 }
 
